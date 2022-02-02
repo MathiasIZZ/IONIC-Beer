d6a13d84887a3b23744be5663605e0764bac026a
Nicolas Gaget <n.gaget69@gmail.com>
2022-02-02 14:59:55 +0100

    refacto services

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Basket } from '../models/basket';
 import { Beer } from '../models/beer';
 
 @Injectable({
@@ -12,22 +11,5 @@ export class BeerService {
 
   public findAll() {
     return this.http.get<Beer[]>(`${environment.apiUrl}/beers`);
-  }
-
-  public findById(id: string) {
-    return this.http.get<Beer>(`${environment.apiUrl}/beers/${id}`);
-  }
-
-  public addToBasket(basketId: string, beer: Beer) {
-    return this.http.post<Basket>(
-      `${environment.apiUrl}/beers/${basketId}`,
-      beer
-    );
-  }
-
-  public deleteFromBasket(basketId: string, beer: Beer) {
-    return this.http.delete<Basket>(
-      `${environment.apiUrl}/beers/${basketId}/${beer.id}`
-    );
   }
 }
