4fc3c277ba9c318ff63cf9d8909838f68184e742
MathiasIZZ <mathias.guiguen@gmail.com>
2022-02-03 09:57:11 +0100

    a little front

---
+++ src/app/intro-app/intro-app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-app',
+  templateUrl: './intro-app.component.html',
+  styleUrls: ['./intro-app.component.scss'],
+})
+export class IntroAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
